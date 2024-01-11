04a8d478f83c255cd36590f8e87da989f620ab21
21120172 <nguyentuandat2k3cmg@gmail.com>
2024-01-11 23:55:26 +0700

    fix video 2

--- MyMediaProject/Pages/HomePage.xaml.cs
+++ MyMediaProject/Pages/HomePage.xaml.cs
@@ -14,6 +14,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Gaming.Input.ForceFeedback;
 using Windows.Media.Core;
 using Windows.Media.Playlists;
 using Windows.Web.Http;
@@ -30,10 +31,10 @@ namespace MyMediaProject.Pages
     public sealed partial class HomePage : Page
     {
         private List<Uri> mediaPlaylist = new List<Uri>();
-       
+
         // To Test HomePage
         public ObservableCollection<Media> MediaCollection { get; set; }
-        public Media SelectedMedia { get; set; }  
+        public Media SelectedMedia { get; set; }
         public HomePage()
         {
             this.InitializeComponent();
@@ -66,10 +67,10 @@ namespace MyMediaProject.Pages
         async private System.Threading.Tasks.Task SetLocalMedia()
         {
             var window = new Microsoft.UI.Xaml.Window();
-            var hwd= WinRT.Interop.WindowNative.GetWindowHandle(window);
+            var hwd = WinRT.Interop.WindowNative.GetWindowHandle(window);
             var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
             //WinRT.Interop.InitializeWithWindow.Initialize(openPicker, WinRT.Interop.WindowNative.GetWindowHandle(this));
-            openPicker.ViewMode=Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            openPicker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
             openPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.MusicLibrary;
 
             openPicker.FileTypeFilter.Add(".wmv");
@@ -92,20 +93,33 @@ namespace MyMediaProject.Pages
             //    mediaPlayerElement.Source = MediaSource.CreateFromUri(mediaPlaylist[currentMediaIndex]);
             //    mediaPlayerElement.MediaPlayer.Play();
 
-                //mediaPlayerElement.Source = MediaSource.CreateFromStorageFile(file);
+            //mediaPlayerElement.Source = MediaSource.CreateFromStorageFile(file);
 
-                //mediaPlayerElement.MediaPlayer.Play();
+            //mediaPlayerElement.MediaPlayer.Play();
 
             var file = await openPicker.PickSingleFileAsync();
             if (file != null)
             {
-                Uri fileUri = new Uri(file.Path);
-                mediaPlaylist.Add(fileUri);
-                //playlist.Items.Add(file.Name);
-                NavigationPage.MainMediaPlayerElement.Source = MediaSource.CreateFromUri(fileUri);
-                NavigationPage.MainMediaPlayerElement.MediaPlayer.Play();
+                var extension = Path.GetExtension(file.Name);
+
+                if (extension.Equals(".mp4") || extension.Equals(".wmv"))
+                {
+                    var subWindow = new Window();
+                    var videoPage = new VideoPage(file);
+                    subWindow.Content = videoPage;
+                    subWindow.Activate();
+                }
+                else 
+                {
+                    Uri fileUri = new Uri(file.Path);
+                    mediaPlaylist.Add(fileUri);
+                    //playlist.Items.Add(file.Name);
+                    NavigationPage.MainMediaPlayerElement.Source = MediaSource.CreateFromUri(fileUri);
+                    NavigationPage.MainMediaPlayerElement.MediaPlayer.Play();
+                }
             }
-            }
+
         }
     }
+}
 
---
+++ MyMediaProject/Pages/VideoPage.xaml.cs
@@ -0,0 +1,39 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Storage;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace MyMediaProject.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class VideoPage : Page
+    {
+        public StorageFile VideoFile { get; set; }
+        public VideoPage(StorageFile videoFile)
+        {
+            this.InitializeComponent();
+            VideoFile = videoFile;
+            Uri fileUri = new Uri(VideoFile.Path);
+            //playlist.Items.Add(file.Name);
+            mediaPlayerElement.Source = MediaSource.CreateFromUri(fileUri);
+            mediaPlayerElement.MediaPlayer.Play();
+        }
+    }
+}
