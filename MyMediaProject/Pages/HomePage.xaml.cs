a7dff75a3b5a1b9c5c7ee944f5f1552c47f8cf0d
21120177 <minhhuy14012003@gmail.com>
2024-01-11 16:24:18 +0700

    Implement add to playlist feature

--- MyMediaProject/Pages/HomePage.xaml.cs
+++ MyMediaProject/Pages/HomePage.xaml.cs
@@ -13,6 +13,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Media.Playlists;
 using Windows.Web.Http;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -20,11 +22,14 @@ using Windows.Web.Http;
 
 namespace MyMediaProject.Pages
 {
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class HomePage : Page
     {
+        private List<Uri> mediaPlaylist = new List<Uri>();
+        private int currentMediaIndex = 0;
         // To Test HomePage
         public ObservableCollection<Media> MediaCollection { get; set; }
         public Media SelectedMedia { get; set; }  
@@ -56,6 +61,44 @@ namespace MyMediaProject.Pages
         {
             public string Image { get; set; }
             public string Name { get; set; }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await SetLocalMedia();
+        }
+
+        async private System.Threading.Tasks.Task SetLocalMedia()
+        {
+            var window = new Microsoft.UI.Xaml.Window();
+            var hwd= WinRT.Interop.WindowNative.GetWindowHandle(window);
+            var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+            //WinRT.Interop.InitializeWithWindow.Initialize(openPicker, WinRT.Interop.WindowNative.GetWindowHandle(this));
+            openPicker.ViewMode=Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            openPicker.FileTypeFilter.Add(".wmv");
+            openPicker.FileTypeFilter.Add(".mp4");
+            openPicker.FileTypeFilter.Add(".wma");
+            openPicker.FileTypeFilter.Add(".mp3");
+            WinRT.Interop.InitializeWithWindow.Initialize(openPicker, hwd);
+
+            var files = await openPicker.PickMultipleFilesAsync();
+
+            // mediaPlayerElement is a MediaPlayerElement control defined in XAML
+            if (files.Count > 0)
+            {
+                foreach (var file in files)
+                {
+                    Uri fileUri = new Uri(file.Path);
+                    mediaPlaylist.Add(fileUri);
+                    //playlist.Items.Add(file.Name);
+                }
+                mediaPlayerElement.Source = MediaSource.CreateFromUri(mediaPlaylist[currentMediaIndex]);
+                mediaPlayerElement.MediaPlayer.Play();
+
+                //mediaPlayerElement.Source = MediaSource.CreateFromStorageFile(file);
+
+                //mediaPlayerElement.MediaPlayer.Play();
+            }
         }
     }
 }
--- MyMediaProject/Pages/PlaylistsPage.xaml.cs
+++ MyMediaProject/Pages/PlaylistsPage.xaml.cs
@@ -8,30 +8,57 @@ using Microsoft.UI.Xaml.Navigation;
 using MyMediaProject.Helpers;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Windows.Media.Playlists;
+using static MyMediaProject.Pages.HomePage;
 
 namespace MyMediaProject.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class PlaylistsPage : Page
     {
+        public ObservableCollection<Playlist> Playlists { get; set; }
+
+        private ContentDialog createPlaylistDialog;
+
         public PlaylistsPage()
         {
             this.InitializeComponent();
+            Playlists = new ObservableCollection<Playlist>();
+
+           
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            await App.MainRoot.ShowCreateDialogDialog("Create Playlist", "Create", "Cancel");
+
+          
+       var res= await App.MainRoot.ShowCreateDialogDialog("Create Playlist", "Create", "Cancel");
+
+            if (!string.IsNullOrEmpty(res))
+            {
+                string playlistName = (createPlaylistDialog.Content as TextBox).Text;
+                if (!string.IsNullOrEmpty(playlistName))
+                {
+                    Playlists.Add(new Playlist { Name = playlistName, MediaCollection = new ObservableCollection<Media>() });
+                }
+            }
         }
+    }
+
+    public class Playlist
+    {
+        public string Name { get; set; }
+        public ObservableCollection<Media> MediaCollection { get; set; }
+    }
+
+    public class Media
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
     }
 }
